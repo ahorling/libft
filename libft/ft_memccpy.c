47eada42a723f107791dd122896e8fea6a6fc39f
alexander <horlingsalexander@gmail.com>
2021-04-12 19:52:30 +0200

    fixing memccpy

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,13 +6,13 @@
 /*   By: alexander <alexander@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/12 19:24:51 by alexander     #+#    #+#                 */
-/*   Updated: 2021/04/12 19:33:27 by alexander     ########   odam.nl         */
+/*   Updated: 2021/04/12 19:52:17 by alexander     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	size_t	i;
 	char	*output;
@@ -21,10 +21,10 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 	i = 0;
 	output = (char *)dest;
 	input = (char *)src;
-	while (i < size)
+	while (i < n)
 	{
 		if (input[i] == c)
-			return (dest[i+1]);
+			return (dest[i + 1]);
 		output[i] = input[i];
 		i++;
 	}
